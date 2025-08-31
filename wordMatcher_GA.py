ac0e233516ca8fe6bea778428c775d3ed5109d69
Shahin <muhammedshahinjaleel@gmail.com>
2025-08-31 19:29:10 +0530

    Update wordMatcher_GA.py

--- wordMatcher_GA.py
+++ wordMatcher_GA.py
@@ -4,7 +4,7 @@ import string
 targetWord = input("Enter a word: ").lower()
 word_length = len(targetWord)
 
-population = 80
+population = 100
 max_gens = 30000
 
 mutation_rate = 0.3
@@ -12,7 +12,7 @@ crossover_rate = 0.5
 
 generation = []
 selectedSet = []
-fitness = []
+fitnessScore = 0
 generation_number = 0
 best_guess = ""
 best_score = -1
@@ -31,11 +31,18 @@ def initiation():
         generation.append(curr_guess)
     print("FIRST GEN: " + str(generation))
 
-
 initiation()
 
-def mutate():
-    pass
+def mutate(population, generation):
+    for p in range(population):
+            old_word = generation[p]
+            letters = []
+            for l in range(word_length):
+                if random.random() >= mutation_rate:
+                    letters.append(old_word[l])
+                else:
+                    letters.append(random.choice(string.ascii_lowercase))
+            generation[p] = ''.join(letters)
 
 def cross(tt):
     split_point = int(round(word_length/2))
@@ -51,18 +58,7 @@ def runLoop():
     global generation
     global best_guess
     while best_guess != targetWord and generation_number < max_gens:
-        for p in range(population):
-            old_word = generation[p]
-            letters = []
-            for l in range(word_length):
-                if random.random() >= mutation_rate:
-                    letters.append(old_word[l])
-                else:
-                    letters.append(random.choice(string.ascii_lowercase))
-            generation[p] = ''.join(letters)
-
-
-        
+        mutate(population, generation)
         calculateFitness()
         generation_number += 1
         #selectedSet.clear()
@@ -72,6 +68,7 @@ def calculateFitness():
     global best_score
     global best_guess
     global top_two
+    global fitnessScore
     for i,ind in enumerate(generation):
         score = 0
         
@@ -94,14 +91,24 @@ def calculateFitness():
             
             if ind not in selectedSet:
                 selectedSet.append(ind)
+        fitnessScore = best_score
 
 def printGuess():
     print("CURRENT: " +str(generation))
 
 runLoop()
 
-print("LAST GEN: " + str(generation))
+final_gen = []
+for l, lg in enumerate(generation):
+    if lg == best_guess:
+        final_gen.append("**" + lg.upper() + "**")
+    else:
+        final_gen.append(lg)
+
+print("LAST GEN: " + str(final_gen))
+
 print("BEST GUESS: " + best_guess)
+print("FITNESS: " + str(fitnessScore))
 print("Generations taken: " + str(generation_number))
 
     
